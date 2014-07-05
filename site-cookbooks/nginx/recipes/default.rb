2915561a16e1c5c125079f079d4c8248acab619e
Daiki Takasao <daiki.takasao@gmail.com>
2014-07-05 11:37:56 +0900

    redisのcookbookを追加

--- site-cookbooks/nginx/recipes/default.rb
+++ site-cookbooks/nginx/recipes/default.rb
@@ -26,3 +26,11 @@ service "nginx" do
   supports :status => true, :restart => true, :reload => true
   action [ :enable, :start ]
 end
+
+template "/etc/nginx/conf.d/default.conf" do
+  source "default.conf.erb"
+  owner "root"
+  group "root"
+  mode 0644
+  notifies :restart, "service[nginx]"  
+end
