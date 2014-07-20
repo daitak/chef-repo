d33e4e7fb0b6783029d59aa90ee47124a391b9de
Daiki Takasao <daiki.takasao@gmail.com>
2014-07-20 17:04:19 +0900

    site-cookbooksをサブモジュール化

--- site-cookbooks/nvm/recipes/express.rb
+++
@@ -1,7 +0,0 @@
-Encoding.default_external = Encoding::UTF_8
-
-%w{ express express-generator}.each do |npm|
-  npm_package npm do
-    action :install
-  end
-end
--- site-cookbooks/redis/attributes/default.rb
+++
@@ -1,2 +0,0 @@
-default['authorization']['sudo']['users']             = ['vagrant', 'redis']
-default['authorization']['sudo']['passwordless']      = true
